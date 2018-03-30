fab19b6db69fc15c337fea5ed363f78d06b805ad
msado <alshaab2000@hotmail.com>
2018-03-30 16:11:27 +0400

    Deleted cwordfunction
    To focus module on measurments

--- functions.py
+++ functions.py
@@ -1,12 +1,3 @@
-a = 'slim salim slime slimey slim.slim'
-def cword(string,word):
-    '''Counts given word in given string and return a integer'''
-    wordlist = string.split()
-    c = 0
-    for x in wordlist:
-            if word in x:
-                    c += 1
-    return c 
 class space():
     def __init__(self,wf,hf,df):
         self.wf = wf
