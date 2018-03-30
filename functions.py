3c0fdd774b11365957f58178babefc7676a0bf00
msado <alshaab2000@hotmail.com>
2018-03-30 16:00:54 +0400

    Class Space created
    Functions arnf (feet) arnm (meters) Created

--- functions.py
+++ functions.py
@@ -7,3 +7,16 @@ def cword(string,word):
             if word in x:
                     c += 1
     return c 
+class space():
+    def __init__(self,wf,hf,df):
+        self.wf = wf
+        self.hf = hf
+        self.df = df
+    def arnf(self):
+        '''Area in squared feet'''
+        r = self.wf * self.df
+        return r
+    def arnm(self):
+        '''Area in squared meters'''
+        r = self.arnf() * 0.092903
+        return r
